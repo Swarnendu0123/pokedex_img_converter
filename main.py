e29ed3a77ad6c704901aa5d43afd87179227ee36
shonuff <jirvine124@gmail.com>
2023-10-02 21:49:24 -0700

    add remove background feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 from PIL import Image
+from rembg.bg import remove
+from tqdm import tqdm
 
 # grab the source_folder and output_folder arguments
 if len(sys.argv) == 3:
@@ -9,7 +11,7 @@ else:
     source_folder, output_folder = sys.argv[1], ""
 
 # opening the folder
-if (os.path.exists(source_folder)):
+if os.path.exists(source_folder):
     files = os.listdir(source_folder)
 else:
     print("No folder found")
@@ -20,10 +22,16 @@ if not os.path.exists(output_folder):
     output_folder = "./converted_images"
 
 # loop througn poke_dex
-for file in files:
-    img = Image.open(source_folder+"/"+file)
-    # convert images to png 
-    file = file.split(".")[0]
-    path = file + ".png"
-    #save the image
-    img.save(os.path.join("./"+output_folder, path))
+if files != 0:
+    print(f"Beginning to remove background from {len(files)} images")
+    for file in files:
+        img = Image.open(source_folder + "/" + file)
+        img = remove(img)
+        print(f"Removing background from {file}")
+        # convert images to png
+        file = file.split(".")[0]
+        path = file + ".png"
+        # save the image
+        img.save(os.path.join("./" + output_folder, path))
+else:
+    print("No files found")
