b8824a49a5db2ecfab6f4903fe12f75c2ee79d46
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2023-10-02 22:39:34 -0700

    Added User Requested Image Type

--- main.py
+++ main.py
@@ -18,12 +18,32 @@ else:
 if not os.path.exists(output_folder):
     os.mkdir("converted_images")
     output_folder = "./converted_images"
+    output_files = os.listdir(source_folder)
+
+filetype = ".png"
+
+print("Select the desired output format. Available Options: ")
+print("     1. PNG")
+print("     2. JPEG")
+print("     3. GIF")
+print("     4. BMP")
+user_choice = input("Enter the number of your choice: ")
+
+#Makes sure user put in a valid option, otherwise converts to PNG
+if (user_choice.isnumeric()):
+    if (user_choice == "2"):
+        filetype = ".jpeg"
+    elif (user_choice == "3"):
+        filetype = ".gif"
+    elif (user_choice == "4"):
+        filetype = ".bmp"
+    
 
 # loop througn poke_dex
 for file in files:
     img = Image.open(source_folder+"/"+file)
     # convert images to png 
     file = file.split(".")[0]
-    path = file + ".png"
+    path = file + filetype
     #save the image
     img.save(os.path.join("./"+output_folder, path))
